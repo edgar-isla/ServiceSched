64548c067b99eba364238ba8866a9526dcec3026
edgar-isla <edgar.isla.2015@gmail.com>
2016-02-10 19:51:38 -0700

    WorkingExample

--- js/app.js
+++ js/app.js
@@ -21,6 +21,42 @@
                     templateUrl: "templates/home.html",
                     controller: "homeController as hc"
                 })
+                .state("alex", {
+                    url: "/alex",
+                    templateUrl: "templates/EmpSched/Alex.html"
+                })
+                .state("brian", {
+                    url: "/brian",
+                    templateUrl: "templates/EmpSched/Brian.html"
+                })
+                .state("britt", {
+                    url: "/britt",
+                    templateUrl: "templates/EmpSched/Britt.html"
+                })
+                .state("colton", {
+                    url: "/colton",
+                    templateUrl: "templates/EmpSched/Colton.html"
+                })
+                .state("dalton", {
+                    url: "/dalton",
+                    templateUrl: "templates/EmpSched/Dalton.html"
+                })
+                .state("edgar", {
+                    url: "/edgar",
+                    templateUrl: "templates/EmpSched/Edgar.html"
+                })
+                .state("malek", {
+                    url: "/malek",
+                    templateUrl: "templates/EmpSched/Malek.html"
+                })
+                .state("josh", {
+                    url: "/josh",
+                    templateUrl: "templates/EmpSched/Josh.html"
+                })
+                .state("Max", {
+                    url: "/Max",
+                    templateUrl: "templates/EmpSched/Max.html"
+                })
                 .state("about", {
                     url: "/about",
                     templateUrl: "templates/about.html"
